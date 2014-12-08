4b636be2bdcad472b832ddcde444ff8258ee5ca2
jyootai <jyootg@gmail.com>
2014-12-08 20:16:30 +0800

    Update 0.0.1

--- lib/sports_events.rb
+++ lib/sports_events.rb
@@ -10,28 +10,30 @@ module SportsEvents
       today=Time.now.day
       date = date.to_i
       id = case date
-           when today				then 2
-           when today+1			then 3
-           when today+2			then 4
-           when today+3  	  then 5 
-           when today+4   	then 6 
-           when today+5   	then 7 
-           when today+6    	then 8 
-           when today+7    	then 9 
-           when today+8    	then 10 
-           when today+9    	then 11 
-           when today+10    then 12 
-           when today+11    then 13 
-           when today+12    then 14 
-           when today+13    then 15 
-           when today+14    then 16 
-           when today+15    then 17 
+           when today      then 2
+           when today+1    then 3
+           when today+2    then 4
+           when today+3    then 5 
+           when today+4    then 6 
+           when today+5    then 7 
+           when today+6    then 8 
+           when today+7    then 9 
+           when today+8    then 10 
+           when today+9    then 11 
+           when today+10   then 12 
+           when today+11   then 13 
+           when today+12   then 14 
+           when today+13   then 15 
+           when today+14   then 16 
+           when today+15   then 17 
            else 0
            end
       if id == 0
-        puts "对不起，暂无赛事数据提供"
+        error = "对不起，暂无赛事数据提供"
+        return error
       else
-        puts doc.xpath("//div[@class='box']")[id].text
+        info = doc.xpath("//div[@class='box']")[id].text
+        return info
       end
     end
   end
