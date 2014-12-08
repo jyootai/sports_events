0a1b8863b34d04c7430314aa49f8d4f6cc7768f6
jyootai <jyootg@gmail.com>
2014-12-08 20:07:13 +0800

    Format adjustment

--- test/query_test.rb
+++ test/query_test.rb
@@ -2,9 +2,9 @@ require 'minitest/autorun'
 require 'sports_events'
 
 class QueryTest < Minitest::Test
-	def test_query
-		date=Time.now.day-2
-		assert_equal "http://www.zhibo8.cc", SportsEvents::Uri
-		assert_equal SportsEvents.query("#{date}"),"对不起，暂无赛事数据提供"
-	end
+  def test_query
+    date=Time.now.day-2
+    assert_equal "http://www.zhibo8.cc", SportsEvents::Uri
+    assert_equal SportsEvents.query("#{date}"),"对不起，暂无赛事数据提供"
+  end
 end
